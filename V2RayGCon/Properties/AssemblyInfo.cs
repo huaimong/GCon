563767ad92b09ae228a303c6b3461d4ab1e87fe0
nobody <nobody@nowhere.mars>
2019-02-15 18:43:45 +0800

    Lock NLua version to 1.3.2.1

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -31,10 +31,11 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.7.17")]
+[assembly: AssemblyVersion("1.1.7.18")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 /*
+ * v1.1.7.18 NLua降级至v1.3.2.1解决中文问题
  * v1.1.7.17 尝试修复Luna输出中文问题失败(KeraLua 5.3导致)
  * v1.1.7.16 改回左键显示主窗口，主窗口菜单添加退出程序功能
  * v1.1.7.15 修复Luna保存无效问题
