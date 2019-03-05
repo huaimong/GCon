c3f2760270d71863b3597406d0d53d0a46eb2511
nobody <nobody@nowhere.mars>
2019-03-05 10:28:40 +0800

    v1.1.9

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -31,25 +31,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.8.16")]
+[assembly: AssemblyVersion("1.1.9.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-/* v1.1.8.16 Editor of Luna plugin support snippets.
- * v1.1.8.15 Luna add function ScanQrcode().
- * v1.1.8.14 主窗口添加复制为vee订阅内容菜单项
- * v1.1.8.13 Fix bug in index refreshing.
- * v1.1.8.12 修复FormQRCode中链接和名称不对应的bug
- * v1.1.8.11 v:// ... 添加 crc8 校验位
- * v1.1.8.10 Enhance v:// ... share link format.
- *              ver 0a support vmess.alterId 
- *              ver 0b support shadowsocks protocol
- * v1.1.8.9 Refactor subscription.
- * v1.1.8.8 把 v:// ... 再缩短了几个字符
- * v1.1.8.7 用v2cfg:// ... 代替 v2ray:// ...
- * v1.1.8.6 v://... 支持quic
- * v1.1.8.5 修复v://...中的一些bugs
- * v1.1.8.4 支持v://...链接(自创的链接)
- * v1.1.8.3 添加ShareLinkMgr服务来处理各种分享链接
- * v1.1.8.2 Refactoring.
- * v1.1.8.1 尝试修复,GetFreeTcpPort()多线程下可能出错的问题
- */
